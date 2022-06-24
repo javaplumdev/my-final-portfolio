074fb8d19b5a574998e23e494624142da0510a7a
Charlito Mecarez Jr <70713746+javaplumdev@users.noreply.github.com>
2022-06-24 21:08:38 +0800

    update

--- portfolio/src/components/ProjectContentPage.jsx
+++ portfolio/src/components/ProjectContentPage.jsx
@@ -4,26 +4,38 @@ import { useParams } from 'react-router-dom';
 // Data
 import { ProjectsData } from '../data/Data';
 import { RecentProjects } from '../data/Data';
+// Bootstrap
+import { Row, Col } from 'react-bootstrap';
 
 const ProjectContentPage = () => {
 	const { id } = useParams();
 
 	const proj = ProjectsData.filter((item) => item.projectID === parseInt(id));
 
-	console.log(ProjectsData);
-	console.log(proj);
-
 	return (
 		<div>
-			<div className="proj-title bg-white p-3">
-				<h1 className="fw-bold">Title</h1>
-			</div>
-			<div className="proj-img">
-				{proj.map((item) => {
-					return (
-						<img key={item.projectID} src={item.image2} className="w-100" />
-					);
-				})}
+			<div>
+				<Row>
+					<Col md="8">
+						{proj.map((item) => {
+							return (
+								<div>
+									<div>
+										<img
+											key={item.projectID}
+											src={item.image2}
+											className="w-100"
+										/>
+										<h4 className="fw-bold mt-3">{item.projectName}</h4>
+									</div>
+								</div>
+							);
+						})}
+					</Col>
+					<Col md="4">
+						<p className="bg-danger">Hi</p>
+					</Col>
+				</Row>
 			</div>
 		</div>
 	);
